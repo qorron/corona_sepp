4dfd049e0bf232442a899809c63333a1e2c268de
quattro <q@qless.org>
2021-02-13 16:58:35 +0100

    add html maker, cleaned up getter

--- bin/get_impfst.pl
+++ bin/get_impfst.pl
@@ -13,10 +13,11 @@ my $url = 'https://pipe.orf.at/corona-dashboard/data/timeline-eimpfpass.csv?orig
 
 my $content = encode('utf8',get($url));
 $content =~ s/1;Gruppe<24_M_1;Gruppe<24_W_1;Gruppe<24_D_1/1;Gruppe<24_M_2;Gruppe<24_W_2;Gruppe<24_D_2/; # fix faulty headers
+$content =~ s/[<>-]/_/g; # not screw up sql
 
 # Connecting to a MySQL database
 my $db = DBIx::Simple->connect(
-    'DBI:Pg:database=corona',    # DBI source specification
+    'DBI:Pg:database=corona;host=127.0.0.1',    # DBI source specification
     $ENV{CORONA_DB_USER},        # Username and password
     $ENV{CORONA_DB_PASS},
     { RaiseError => 1 }          # Additional options
@@ -27,22 +28,23 @@ my $aoh = csv(
 	#headers => "auto",
 	detect_bom => 1,
 	sep_char   => ";",
-	munge_column_names => "none",
+#	munge_column_names => "none",
 );    # as array of hash
 
 my @seen;
 for my $row (@$aoh) {
 	@seen =
-		$db->select( 'impfst', 'DISTINCT Datum', { Datum => $row->{Datum}, BundeslandID => $row->{BundeslandID} } )->hashes;
+		$db->select( 'impfst', 'DISTINCT Datum', { Datum => $row->{datum}, BundeslandID => $row->{bundeslandid} } )->hashes;
 	if (@seen) {
 		# warn "skipping $row->{BundeslandID} $row->{Datum}";
 		next;
 	}
-	say "inserting $row->{BundeslandID} $row->{Datum}";
+	say "inserting $row->{bundeslandid} $row->{datum}";
 	my $qt_row = {};
 	for my $key (keys %$row) {
 		$qt_row->{qq'"$key"'} = $row->{$key};
-		say $key;
+		delete $row->{$key} if $row->{$key} eq ''; # integer
 	}
-	$db->insert( 'impfst', {%$qt_row} );
+	warn __PACKAGE__.':'.__LINE__.$".Data::Dumper->Dump([\$row], ['row']);
+	$db->insert( 'impfst', {%$row} );
 }
---
+++ bin/make_impfst.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use 5.020;
+use Data::Dumper;
+
+use DBIx::Simple;
+use Date::Parse;
+use POSIX qw(strftime);
+use File::Slurper qw(write_text);
+
+my $file = '/var/www/impfst.at/index.html';
+
+# Connecting to a MySQL database
+my $db = DBIx::Simple->connect(
+	'DBI:Pg:database=corona;host=127.0.0.1',    # DBI source specification
+	$ENV{CORONA_DB_USER},                       # Username and password
+	$ENV{CORONA_DB_PASS},
+	{ RaiseError => 1 }                         # Additional options
+);
+my @fresh_data = $db->select( 'impfst_28d_done', 'teilgeimpfte_est_date', { BundeslandID => 10 } )->hashes;
+if (@fresh_data) {
+	my $time = str2time( $fresh_data[0]->{teilgeimpfte_est_date} );
+
+	#say strftime('%d.%m.%Y', localtime($time));
+	my $done_date = strftime( '%d.%m.%Y',       localtime($time) );
+	my $now_date  = strftime( '%d.%m.%Y %H:%M', localtime() );
+	my $html      = <<XXX;
+<html>
+<head>
+</head>
+<body>
+Based upon the data available from <a href="https://orf.at/">orf.at</a> all Austrians will recieve their first SARS Cov2 jab by this date:<br>
+<h1>$done_date</h1>
+This was calculated extrapolating the vaccination progress over the last 28 days.<br>
+Last updated: $now_date (once a day unless something goes wrong)
+This site is not affiliated with anyone and was only created to take my mind off the fact that a 'wird scho nix sein' person I recently met was tested positive the very next day. Source: <a href="https://github.com/qorron/corona_sepp">github</a>
+</body>
+</html>
+XXX
+	write_text( $file, $html );
+
+}
+else {
+	warn "update failed!";
+}
+
+
